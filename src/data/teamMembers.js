0da49a5df9620fe9bcf69868317cdbbf049aa8aa
iverkroken <iverkroken@gmail.com>
2026-04-23 18:23:09 +0200

    feat: add team images and update teamMembers data

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -2,31 +2,31 @@ export const teamMembers = [
   {
     id: 1,
     name: 'Iver Kroken',
-    image: '/images/team/iver.jpg',
+    image: '/images/iver-linkedin.jpg',
     linkedin: 'https://www.linkedin.com/in/iver-kroken/',
   },
   {
     id: 2,
     name: 'Tobias Olsen Nodland',
-    image: '/images/team/tobias.jpg',
+    image: '/images/tobias-linkedin.jpg',
     linkedin: 'https://www.linkedin.com/in/tobias-olsen-nodland-44b03a3a0/',
   },
   {
     id: 3,
     name: 'Sivert Svanes Sæstad',
-    image: '',
+    image: '/images/sivert-linkedin.jpg',
     linkedin: 'https://www.linkedin.com/in/sivert-svanes-s%C3%A6stad-615aa1262/',
   },
   {
     id: 4,
     name: 'Eira Bitnes Vikestøl',
-    image: '/images/team/eira.jpg',
+    image: '/images/Eira-linkedin.jpg',
     linkedin: 'https://www.linkedin.com/in/eira-bitnes-vikst%C3%B8l-185136355/',
   },
   {
     id: 5,
     name: 'Oda Elise Aanestad',
-    image: '/images/team/oda.jpg',
+    image: '/images/oda-linkedin.jpg',
     linkedin: 'https://www.linkedin.com/in/oda-elise-aanestad-857206351/',
   },
 ]

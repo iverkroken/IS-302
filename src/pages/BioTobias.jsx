e8a3d87842d4030bda7ad5dabce76411e9b4e82c
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-04-27 14:13:32 +0200

    feat: tobias page done

--- src/pages/BioTobias.jsx
+++ src/pages/BioTobias.jsx
@@ -6,6 +6,7 @@ import BioCategoryWrapper from '../components/BioCategoryWrapper'
 import ImageCarousel from '../components/ImageCarousel'
 import Projects from '../components/Projects'
 import BioPageWrapper from '../components/BioPageWrapper'
+import BioTextSection from '../components/BioTextSection'
 import './Page.css'
 import './BioPage.css'
 
@@ -59,10 +60,21 @@ export default function BioTobias() {
 
           <div className="bio-content">
             <BioCategoryWrapper>
-              <BioImageSection title="Mine interesser" image={`${import.meta.env.BASE_URL}images/tobias-linkedin.jpg`}>
-                <p>Jeg er liker å lære og forstå hvordan ting fungerer fra grunnen, slik at problemer kan løses på en informert og effektiv måte. Jeg er spesielt interessert i å utvikle løsninger med god en brukeropplevelse både for brukere og medarbeidere. Veldig interessert i systemprogrammering</p>
+              <BioImageSection title="Mine interesser" image={`${import.meta.env.BASE_URL}images/tobias/9e529c5b-f66c-4eb5-8f65-aec4f91d0773.jpg`}>
+                <p>Jeg har mange i interesser, men en av de største er interessen min i teknologi. Den dekker egentlig alt fra spill til programmering,
+                  og er noe som jeg alltid har vært veldig interessert i.</p>
+                <p> Utenom teknologi er jeg også veldig glad i matlaging, fordi jeg liker mat og da er det jo ganske nødvendig
+                  å ta seg tid til å lage god mat. Jeg er spesielt glad i det franske og italienske kjøkkenet,
+                  fra det franske kjøkkenet ville jeg sagt at det er biffrettene og sausene som er mine favoritter,
+                  det er lite som slår en entrecote med en madeirasaus(fiananciere). Fra Italia er absolutt napolitansk pizza
+                  og carbonara som er favoritten, og da mener jeg carbonara laget med pecorino, egg og guanciale.</p>
               </BioImageSection>
-              <br/>
+              <BioTextSection>
+                <p>Jeg er også veldig glad i fotografi, og har som regel med meg kamera når jeg på tur eller fisker, men
+                det er også ofte jeg drar ut med mål å bare ta bilder. Jeg pleier å ta bilder først og fremst på en analog
+                Nikon F3 fordi jeg syntes det er lite som kan måle seg med tonene en får fra film. Men jeg tar også mye
+                bilder på digtal.</p>
+              </BioTextSection>
               <br/>
               <BioImageSection title="Foto galleri">
                 <ImageCarousel images={tobiasImages} title="Tobias" />

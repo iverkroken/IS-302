7776432e780ffded033307214c0d7e9139037db8
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-04-27 04:31:16 +0200

    feat: update header for team

---
+++ src/pages/BioEira.jsx
@@ -0,0 +1,34 @@
+﻿import { useNavigate } from 'react-router-dom'
+import { teamMembers } from '../data/teamMembers'
+import BioHeader from '../components/BioHeader'
+import './Page.css'
+import './BioPage.css'
+import BioCategoryWrapper from "../components/BioCategoryWrapper.jsx";
+
+export default function BioEira() {
+  const navigate = useNavigate()
+  const member = teamMembers.find(m => m.id === 4)
+
+  if (!member) {
+    return <div>Teammedlem ikke funnet</div>
+  }
+
+  return (
+    <div className="page">
+      <div className="container">
+        <button className="btn-back" onClick={() => navigate('/team')}>
+          ← Tilbake til teamet
+        </button>
+
+        <BioHeader member={member} />
+
+        <div className="bio-content">
+          <BioCategoryWrapper>
+            {/* bruk denne hvis du skal ha mer i ting i bion, passer basically på at seperator linja er på bunnen */}
+          </BioCategoryWrapper>
+        </div>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/pages/BioSivert.jsx
@@ -0,0 +1,34 @@
+﻿import { useNavigate } from 'react-router-dom'
+import { teamMembers } from '../data/teamMembers'
+import BioHeader from '../components/BioHeader'
+import './Page.css'
+import './BioPage.css'
+import BioCategoryWrapper from '../components/BioCategoryWrapper'
+
+export default function BioSivert() {
+  const navigate = useNavigate()
+  const member = teamMembers.find(m => m.id === 3)
+
+  if (!member) {
+    return <div>Teammedlem ikke funnet</div>
+  }
+
+  return (
+    <div className="page">
+      <div className="container">
+        <button className="btn-back" onClick={() => navigate('/team')}>
+          ← Tilbake til teamet
+        </button>
+
+        <BioHeader member={member} />
+
+        <div className="bio-content">
+          <BioCategoryWrapper>
+
+          </BioCategoryWrapper>
+        </div>
+      </div>
+    </div>
+  )
+}
+
